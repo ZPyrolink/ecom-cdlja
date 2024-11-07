e1b145b1357abe343b47a26a5eb625a0c21b5aee
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-07 16:44:06 +0100

    add vetement type in filter menu

--- src/main/webapp/app/component/filter-menu/filtrer-menu.component.ts
+++ src/main/webapp/app/component/filter-menu/filtrer-menu.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
+import { ClothesTypeEnum } from '../../enums/clothes-type-enum';
 
 @Component({
   selector: 'jhi-filter-menu',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgForOf],
   templateUrl: './filter-menu.component.html',
   styleUrl: './filter-menu.component.scss',
 })
-export default class FilterMenuComponent {}
+export default class FilterMenuComponent {
+  typesOfClothes = Object.values(ClothesTypeEnum);
+}
