0c4fdbc476b2bdbe10b262f077d95a6b95d1880e
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-08 16:28:59 +0100

    add of general components for selected items and checkbox  list

--- src/main/webapp/app/component/filter-menu/filtrer-menu.component.ts
+++ src/main/webapp/app/component/filter-menu/filtrer-menu.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NgForOf, NgIf } from '@angular/common';
 import { ClothesTypeEnum } from '../../enums/clothes-type-enum';
+import SelectedItemsComponent from '../selected-items/selected-items.component';
+import CheckboxListComponent from '../checkbox-list/checkbox-list.component';
 
 @Component({
   selector: 'jhi-filter-menu',
   standalone: true,
-  imports: [NgIf, NgForOf],
+  imports: [NgIf, NgForOf, SelectedItemsComponent, CheckboxListComponent],
   templateUrl: './filter-menu.component.html',
   styleUrl: './filter-menu.component.scss',
 })
@@ -13,18 +15,15 @@ export default class FilterMenuComponent {
   typesOfClothes = Object.values(ClothesTypeEnum);
   selectedItems: string[] = [];
 
-  toggleSelection(type: string, event: Event): void {
-    const checkbox = event.target as HTMLInputElement;
-    if (checkbox.checked) {
-      this.selectedItems.push(type);
-    } else {
+  toggleSelection(type: string): void {
+    if (this.selectedItems.includes(type)) {
       this.selectedItems = this.selectedItems.filter(item => item !== type);
+    } else {
+      this.selectedItems.push(type);
     }
   }
 
   removeItem(item: string): void {
     this.selectedItems = this.selectedItems.filter(selected => selected !== item);
-    const checkbox = document.getElementById(`checkbox-${item}`) as HTMLInputElement;
-    checkbox.checked = false;
   }
 }
