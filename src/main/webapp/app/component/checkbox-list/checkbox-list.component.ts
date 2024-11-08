6f253c225c3f9713898a7f19cbbe74c562ded1a4
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-08 15:17:31 +0100

    create component

---
+++ src/main/webapp/app/component/checkbox-list/checkbox-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'jhi-checkbox-list',
+  standalone: true,
+  imports: [NgForOf],
+  templateUrl: './checkbox-list.component.html',
+  styleUrl: './checkbox-list.component.scss',
+})
+export class CheckboxListComponent {
+  @Input() items: string[] = [];
+  @Input() selectedItems: string[] = [];
+  @Output() toggleSelection = new EventEmitter<string>();
+
+  isChecked(item: string): boolean {
+    return this.selectedItems.includes(item);
+  }
+
+  onToggleSelection(item: string, event: Event): void {
+    this.toggleSelection.emit(item);
+  }
+}
---
+++ src/main/webapp/app/component/selected-items/selected-items.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectedItemsComponent } from './selected-items.component';
+
+describe('SelectedItemsComponent', () => {
+  let component: SelectedItemsComponent;
+  let fixture: ComponentFixture<SelectedItemsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SelectedItemsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SelectedItemsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
