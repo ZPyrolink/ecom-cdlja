21635d22ab3f26f45833711152178fdc414ea60a
Corenthin ZOZOR <corenthin.zozor@gmail.com>
2024-11-14 16:40:59 +0100

    Add many to many test

--- src/main/webapp/app/component/note-form/note-form.component.ts
+++ src/main/webapp/app/component/note-form/note-form.component.ts
@@ -7,6 +7,9 @@ import { FormsModule } from '@angular/forms';
 import { NoteType } from '../../entities/enumerations/note-type.model';
 import { KeyValuePipe } from '@angular/common';
 import { EntityResponseType } from '../../entities/user/service/user.service';
+import { NewLine } from '../../entities/line/line.model';
+import { LineService, PartialUpdateLine } from '../../entities/line/service/line.service';
+import { forkJoin, Observable } from 'rxjs';
 
 @Component({
   selector: 'jhi-note-form',
@@ -22,11 +25,14 @@ export class NoteFormComponent implements OnInit {
     type: null,
   };
 
+  content: (NewLine | PartialUpdateLine)[] = [];
+
   protected readonly NoteType = NoteType;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private service: NoteService,
+    private lineService: LineService,
     private router: Router,
   ) {}
 
@@ -34,6 +40,7 @@ export class NoteFormComponent implements OnInit {
     this.activatedRoute.params.pipe(map(params => params['id'] as number)).subscribe(id => {
       if (id) {
         this.service.find(id).subscribe(value => (this.note = value.body!));
+        this.lineService.ofNote(id).subscribe(value => (this.content = value.body!));
       }
     });
   }
@@ -43,12 +50,25 @@ export class NoteFormComponent implements OnInit {
       this.router.navigate(['poc']);
     };
 
+    const observables: Observable<EntityResponseType>[] = [];
+
     if (this.note.id == null) {
       window.console.log('Creating');
-      this.service.create(this.note).subscribe(nav);
+      observables.push(this.service.create(this.note));
     } else {
       window.console.log('Updating');
-      this.service.partialUpdate(this.note).subscribe(nav);
+      observables.push(this.service.partialUpdate(this.note));
     }
+
+    for (const line of this.content) {
+      if (line.id == null) {
+        observables.push(this.lineService.create(line));
+      } else {
+        observables.push(this.lineService.partialUpdate(line));
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    forkJoin(observables).subscribe(() => this.router.navigate(['poc']));
   }
 }
