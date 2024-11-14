44f9ad90c26fbab4512f07398a1b6dfbb25f392b
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-14 14:09:15 +0100

    add enum for color

---
+++ src/main/webapp/app/enums/color-enum.ts
@@ -0,0 +1,14 @@
+export enum ColorEnum {
+  Vert = 'Vert',
+  Noir = 'Noir',
+  Blanc = 'Blanc',
+  Beige = 'Beige',
+  Rouge = 'Rouge',
+  Bleu = 'Bleu',
+  Violet = 'Violet',
+  Jaune = 'Jaune',
+  Orange = 'Orange',
+  Rose = 'Rose',
+  Gris = 'Gris',
+  Marron = 'Marron',
+}
