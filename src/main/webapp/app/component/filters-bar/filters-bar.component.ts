ce27fd38aaa537e68e6c07f611caa8acf1034b9d
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-15 13:55:06 +0100

    creation color item component

--- src/main/webapp/app/component/filters-bar/filters-bar.component.ts
+++ src/main/webapp/app/component/filters-bar/filters-bar.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { NgForOf, NgIf } from '@angular/common';
-import { ColorFilterComponent } from './color-filter/color-filter.component';
+import { ColorItemComponent } from '../color-item/color-item.component';
 import { SizeFilterComponent } from './size-filter/size-filter.component';
 import { PriceFilterComponent } from './price-filter/price-filter.component';
 import { GenderFilterComponent } from './gender-filter/gender-filter.component';
+import { ColorEnum } from '../../enums/color-enum';
 
 @Component({
   selector: 'jhi-filters-bar',
   standalone: true,
-  imports: [NgForOf, NgIf, ColorFilterComponent, SizeFilterComponent, PriceFilterComponent, GenderFilterComponent],
+  imports: [NgForOf, NgIf, ColorItemComponent, SizeFilterComponent, PriceFilterComponent, GenderFilterComponent],
   templateUrl: './filters-bar.component.html',
   styleUrl: './filters-bar.component.scss',
 })
 export class FiltersBarComponent {
   showColorFilter: string | undefined = undefined;
+  protected readonly Object = Object;
+  protected readonly ColorEnum = ColorEnum;
 
   onFilter(filterType: string): void {
     if (this.showColorFilter === filterType) {
