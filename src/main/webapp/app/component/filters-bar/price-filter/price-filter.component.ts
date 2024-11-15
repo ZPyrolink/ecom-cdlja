df326ce3beaf2449bf7864e844828993c8e65044
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-15 16:56:15 +0100

    add size filter

--- src/main/webapp/app/component/filters-bar/price-filter/price-filter.component.ts
+++ src/main/webapp/app/component/filters-bar/price-filter/price-filter.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'jhi-price-filter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './price-filter.component.html',
   styleUrl: './price-filter.component.scss',
 })
-export class PriceFilterComponent {}
+export class PriceFilterComponent {
+  minValue = 0;
+  maxValue = 100;
+
+  updateMin(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const min = parseInt(target.value, 10);
+    this.minValue = Math.min(min, this.maxValue - 1); // Assure que minValue reste inférieur à maxValue
+  }
+
+  updateMax(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const max = parseInt(target.value, 10);
+    this.maxValue = Math.max(max, this.minValue + 1); // Assure que maxValue reste supérieur à minValue
+  }
+}
