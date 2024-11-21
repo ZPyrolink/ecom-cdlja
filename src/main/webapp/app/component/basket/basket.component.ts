99e915e6a16cf6f932f40c8978991c7068d132a0
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-21 15:23:41 +0100

    test request back

--- src/main/webapp/app/component/basket/basket.component.ts
+++ src/main/webapp/app/component/basket/basket.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'jhi-basket',
   standalone: true,
-  imports: [],
+  imports: [PaginationComponent, NgForOf],
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.scss',
 })
-export default class BasketComponent {}
+export default class BasketComponent {
+  products = {
+    name: 'Brassiere',
+    size: 'Small',
+    color: 'Rose',
+    price: 20,
+    quantity: 1,
+    image: 'https://via.placeholder.com/150',
+  };
+  deliveryPrice = 5.99;
+  totalPrice = 10;
+
+  totalOrderPrice(): any {
+    return this.deliveryPrice;
+  }
+
+  increaseQuantity(product: any): void {
+    product.quantity++;
+  }
+
+  decreaseQuantity(product: any): void {
+    if (product.quantity > 1) {
+      product.quantity--;
+    }
+  }
+
+  editProduct(product: any): void {
+    alert(`Modifier le produit : ${product.name}`);
+  }
+
+  submitOrder(): void {
+    alert('Commande soumise avec succès !');
+  }
+}
