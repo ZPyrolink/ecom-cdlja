5ae5d1aac9cc4fc1a9cdfb07f21f25c3892b6f73
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-21 10:18:48 +0100

    add base listing product

--- src/main/webapp/app/component/listing-product/listing-product.component.ts
+++ src/main/webapp/app/component/listing-product/listing-product.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import SharedModule from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { FiltersBarComponent } from '../filters-bar/filters-bar.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'jhi-listing-product',
   standalone: true,
   templateUrl: './listing-product.component.html',
   styleUrl: './listing-product.component.scss',
-  imports: [SharedModule, RouterModule, FiltersBarComponent],
+  imports: [SharedModule, RouterModule, FiltersBarComponent, PaginationComponent],
 })
-export default class ListingProductComponent {}
+export default class ListingProductComponent {
+  totalPages = 5;
+  currentPage = 1;
+  onPageChange(page: number): void {
+    this.currentPage = page;
+  }
+}
