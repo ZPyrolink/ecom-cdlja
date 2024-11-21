302f803a45e57bba3498bdb7c09f17138cabd324
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-11-21 14:45:16 +0100

    test request back

--- src/main/webapp/app/component/listing-product/listing-product.component.ts
+++ src/main/webapp/app/component/listing-product/listing-product.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import SharedModule from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { FiltersBarComponent } from '../filters-bar/filters-bar.component';
 import { PaginationComponent } from '../pagination/pagination.component';
+import { StockService } from '../../entities/stock/service/stock.service';
+import { IStock } from '../../entities/stock/stock.model';
 
 @Component({
   selector: 'jhi-listing-product',
@@ -11,9 +13,16 @@ import { PaginationComponent } from '../pagination/pagination.component';
   styleUrl: './listing-product.component.scss',
   imports: [SharedModule, RouterModule, FiltersBarComponent, PaginationComponent],
 })
-export default class ListingProductComponent {
+export default class ListingProductComponent implements OnInit {
   totalPages = 5;
   currentPage = 1;
+  stocks: IStock[] = [];
+  constructor(private service: StockService) {}
+
+  ngOnInit(): void {
+    this.service.query().subscribe(next => (this.stocks = next.body ?? []));
+  }
+
   onPageChange(page: number): void {
     this.currentPage = page;
   }
