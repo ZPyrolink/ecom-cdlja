fd5ceab3eb4a3a47ae74568fe6533ef5144a37ac
ndayonga <aimetresorndayo@gmail.com>
2024-11-22 14:46:44 +0100

    Update BasketService and BasketResource

--- src/main/java/fr/cdlja/weebsport/web/rest/BasketResource.java
+++ src/main/java/fr/cdlja/weebsport/web/rest/BasketResource.java
@@ -1,6 +1,7 @@
 package fr.cdlja.weebsport.web.rest;
 
 import fr.cdlja.weebsport.service.BasketService;
+import fr.cdlja.weebsport.service.dto.OrderDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,20 +19,20 @@ public class BasketResource {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> ajouterArticle(@PathVariable(value = "id", required = false) final long articleId) {
+    public ResponseEntity<OrderDTO> ajouterArticle(@PathVariable(value = "id", required = false) final long articleId) {
         try {
-            basketService.ajouterArticle(articleId);
-            return ResponseEntity.ok().build();
+            OrderDTO panier = basketService.ajouterArticle(articleId);
+            return ResponseEntity.ok(panier);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // 200 OK
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> supprimerArticle(@PathVariable(value = "id", required = false) final long articleId) {
+    public ResponseEntity<OrderDTO> supprimerArticle(@PathVariable(value = "id", required = false) final long articleId) {
         try {
-            basketService.supprimerArticle(articleId);
-            return ResponseEntity.ok().build();
+            OrderDTO panier = basketService.supprimerArticle(articleId);
+            return ResponseEntity.ok(panier);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // 200 OK
         }
