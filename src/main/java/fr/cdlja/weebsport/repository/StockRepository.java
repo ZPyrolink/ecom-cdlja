c1c34beff31dc566e83ead88b92559929841005e
bouvieli <129963841+bouvieli@users.noreply.github.com>
2024-11-28 15:46:36 +0100

    Recupération coleurs et tailles
    Pour un vetement donné récupération des couleurs disponibles.
    Pour un vetement et une couleur donnés récupération des tailles dispo

--- src/main/java/fr/cdlja/weebsport/repository/StockRepository.java
+++ src/main/java/fr/cdlja/weebsport/repository/StockRepository.java
@@ -2,6 +2,9 @@ package fr.cdlja.weebsport.repository;
 
 import fr.cdlja.weebsport.domain.Clothe;
 import fr.cdlja.weebsport.domain.Stock;
+import fr.cdlja.weebsport.domain.enumeration.Color;
+import fr.cdlja.weebsport.domain.enumeration.Size;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
@@ -19,4 +22,10 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     @Query("SELECT s FROM Stock s WHERE s.quantity > 0")
     Page<Stock> findStocksWithQuantityGreaterThanZero(Pageable pageable);
+
+    @Query("SELECT s.size FROM Stock s WHERE s.clothe.id = :clotheId AND s.color = :color AND s.quantity > 0")
+    List<Size> findAvailableSizesByClotheIdAndColorId(@Param("clotheId") Long clotheId, @Param("color") Color color);
+
+    @Query("SELECT s.color FROM Stock s WHERE s.clothe.id= :clotheId AND s.quantity > 0 ")
+    List<Color> findAvailableColorsByClotheId(@Param("clotheId") Long clotheId);
 }
