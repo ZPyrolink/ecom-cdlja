3e343d63091f1f66b43cf86913a1f78753e11e5f
Corenthin ZOZOR <corenthin.zozor@gmail.com>
2024-11-29 10:10:29 +0100

    Finish form

--- src/main/webapp/app/component/payement/payement.component.ts
+++ src/main/webapp/app/component/payement/payement.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { faCcApplePay } from '@fortawesome/free-brands-svg-icons';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RegexpUtils } from '../../utils/regexp';
+
+function validators(required = true, pattern?: RegExp, ...others: ValidatorFn[]): ValidatorFn[] {
+  const result: ValidatorFn[] = [];
+
+  if (required) {
+    result.push(Validators.required);
+  }
+
+  if (pattern) {
+    result.push(Validators.pattern(pattern));
+  }
+
+  if (others.length > 0) {
+    result.push(...others);
+  }
+
+  return result;
+}
+
+function onValueChanged(control: FormControl, callback: (value: string) => string): void {
+  control.valueChanges.subscribe((val: string) => {
+    control.setValue(callback(val), { emitEvent: false });
+  });
+}
 
 @Component({
   selector: 'jhi-payement',
@@ -12,46 +46,89 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
   styleUrl: './payement.component.scss',
 })
 export default class PayementComponent {
-  static MAX_CARD_NB_SIZE = 16;
-  static CARD_NB_GROUP_SIZE = 4;
-
   price = 0;
 
-  private cardNumControl = new FormControl('', {
-    nonNullable: true,
-    validators: [Validators.required, Validators.maxLength(PayementComponent.MAX_CARD_NB_SIZE)],
-  });
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  payementForm = new FormGroup({
-    type: new FormControl('residence', { validators: [Validators.required] }),
-    firstName: new FormControl('', { validators: [Validators.required] }),
-    lastName: new FormControl('', { validators: [Validators.required] }),
-    cardNum: this.cardNumControl,
-  });
+  test = '';
 
-  // eslint-disable-next-line @typescript-eslint/member-ordering
   protected readonly faCcApplePay = faCcApplePay;
 
+  protected cardNumControl = new FormControl('', {
+    nonNullable: true,
+    validators: validators(true, RegexpUtils.CARD_NUM),
+  });
+
+  protected expirationDateControl = new FormControl('', {
+    nonNullable: true,
+    validators: validators(true, RegexpUtils.EXPIRATION_DATE, expirationDateValidator),
+  });
+
+  protected cryptoControl = new FormControl('', {
+    nonNullable: true,
+    validators: validators(true, RegexpUtils.CRYPTO),
+  });
+
+  protected phoneNumControl = new FormControl('', {
+    nonNullable: true,
+    validators: validators(true, RegexpUtils.PHONE_NUM),
+  });
+
+  protected payementForm = new FormGroup({
+    type: new FormControl('residence', { validators: validators() }),
+    firstName: new FormControl('', { validators: validators() }),
+    lastName: new FormControl('', { validators: validators() }),
+    email: new FormControl('', { validators: validators(true, undefined, Validators.email) }),
+    country: new FormControl('', { validators: validators() }),
+    expirationDate: this.expirationDateControl,
+    city: new FormControl('', { validators: validators() }),
+    address: new FormControl('', { validators: validators() }),
+    phoneNumber: this.phoneNumControl,
+
+    cardName: new FormControl('', { validators: validators(true, RegexpUtils.CARD_NAME) }),
+    cardNum: this.cardNumControl,
+    crypto: this.cryptoControl,
+    postalCode: new FormControl('', { validators: validators(true, RegexpUtils.POSTAL_CODE) }),
+  });
+
   constructor() {
-    this.cardNumControl.valueChanges.subscribe((val: string) => {
-      val = this.formatCardNum(val);
-      this.cardNumControl.setValue(val, { emitEvent: false });
-    });
+    onValueChanged(this.cardNumControl, this.groupNumbers(4));
+    onValueChanged(this.expirationDateControl, this.keepNumbers);
+    onValueChanged(this.cryptoControl, this.keepNumbers);
+    onValueChanged(this.phoneNumControl, this.groupNumbers(2));
   }
 
   pay(): void {
     window.console.log('payed');
   }
 
-  formatCardNum(cardNumber: string): string {
-    const tmp = cardNumber.replace(/[^0-9]+/g, '');
-
-    if (tmp.length > PayementComponent.MAX_CARD_NB_SIZE) {
-      return this.formatCardNum(tmp.substring(0, PayementComponent.MAX_CARD_NB_SIZE));
-    }
-
-    const regexp = `(.{1,${PayementComponent.CARD_NB_GROUP_SIZE}})`;
-    const matches = tmp.match(new RegExp(regexp, 'g')) ?? [tmp];
-    return matches.join(' ');
+  private groupNumbers(groupSize: number, separator = ' '): (v: string) => string {
+    return (value: string): string => {
+      const tmp = this.keepNumbers(value);
+      const regexp = `(.{1,${groupSize}})`;
+      const matches = tmp.match(new RegExp(regexp, 'g')) ?? [value];
+      return matches.join(separator);
+    };
   }
+
+  private keepNumbers(value: string): string {
+    return value.replace(/[^0-9/]/g, '');
+  }
+}
+
+function expirationDateValidator(control: AbstractControl<string>): ValidationErrors | null {
+  const [month, year] = control.value.split('/').map(v => +v);
+
+  const error = { expiredCard: true };
+
+  const today = new Date();
+  const currentYear = today.getFullYear() - 2000;
+
+  if (year < currentYear) {
+    return error;
+  }
+
+  if (year === currentYear && month - 1 < today.getMonth()) {
+    return error;
+  }
+
+  return null;
 }
---
+++ src/main/webapp/app/utils/regexp.ts
@@ -0,0 +1,8 @@
+export class RegexpUtils {
+  static POSTAL_CODE = /^\d{5}$/;
+  static PHONE_NUM = /^0[1-9](?:\s\d{2}){4}$/;
+  static CRYPTO = /^\d{3}$/;
+  static EXPIRATION_DATE = /^(?:0?[0-9]|1[0-2])\/\d{2}$/;
+  static CARD_NUM = /^(?:\s\d{4}){3}\d{4}$/;
+  static CARD_NAME = /^[a-zA-Z-]+\s[a-zA-Z-]+$/g;
+}
