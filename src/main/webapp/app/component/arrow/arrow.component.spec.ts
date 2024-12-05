bff2c0957e231e00f96d9e9eb909e5a461600cae
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-12-05 10:14:38 +0100

    fix tests

--- src/main/webapp/app/component/arrow/arrow.component.spec.ts
+++ src/main/webapp/app/component/arrow/arrow.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ArrowComponent } from './arrow.component';
+import ArrowComponent from './arrow.component';
 
 describe('ArrowComponent', () => {
   let component: ArrowComponent;
