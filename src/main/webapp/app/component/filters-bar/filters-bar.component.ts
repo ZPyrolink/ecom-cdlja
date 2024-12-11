31cb06b5b436b7a54805e7f2d1e04f43d1369ea1
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2024-12-11 10:58:43 +0100

    filter works

--- src/main/webapp/app/component/filters-bar/filters-bar.component.ts
+++ src/main/webapp/app/component/filters-bar/filters-bar.component.ts
@@ -27,6 +27,7 @@ export class FiltersBarComponent implements OnInit {
     gender: [],
     sort: '',
   };
+  selectedOption = '';
   protected readonly Object = Object;
   protected readonly ColorEnum = ColorEnum;
   protected readonly SizeEnum = SizeEnum;
@@ -50,6 +51,7 @@ export class FiltersBarComponent implements OnInit {
     this.selections['sort'] = option;
     this.filtersChanged.emit(this.selections);
     this.filterService.setSort(option);
+    this.selectedOption = option;
   }
 
   updateSelection(filterType: FilterType, selection: any): void {
